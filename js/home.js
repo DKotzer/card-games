be7c4386010ef92323424618f2fcdf458aa103a9
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-02-24 18:52:05 -0500

    changing home.html to index.html

--- js/home.js
+++ js/home.js
@@ -1,28 +1,20 @@
 /*----- constants -----*/
-
 /*----- app's state (variables) -----*/
-
 /*----- cached element references -----*/
 
 let warEl = document.querySelector(".war-container");
 let blackjackEl = document.querySelector(".blackjack-container");
-let videoPokerEl = document.querySelector(".videoPoker-container");
 
 /*----- event listeners -----*/
 warEl.addEventListener("click", warGame);
 blackjackEl.addEventListener("click", blackjackGame);
-videoPokerEl.addEventListener("click", videoPokerGame);
 
 /*----- functions -----*/
 
 function warGame() {
-  location.href = "index.html";
+  location.href = "war.html";
 }
 
 function blackjackGame() {
-  location.href = "blackjack.html";
-}
-
-function videoPokerGame() {
   location.href = "blackjack.html";
 }
