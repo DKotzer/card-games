d02046aa77819ff3909593217d295d15429e4cfe
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-02-23 19:50:36 -0500

    working game over modal

--- js/blackjack.js
+++ js/blackjack.js
@@ -416,7 +416,7 @@ function dealerTurn() {
     checkWinner();
     dealer.turnComplete = true;
     console.log("Dealer over 16");
-    modal();
+    modals();
     setTimeout(() => resetHands(), 2500);
   }
 }
@@ -481,7 +481,7 @@ function render() {
   cashEl.textContent = player.bank;
 }
 
-function modal() {
+function modals() {
   modalEl.classList.remove("hidden");
   setTimeout(() => modalEl.classList.add("hidden"), 3000);
   betModalEl.classList.remove("hidden");
@@ -547,12 +547,40 @@ function shuffle() {
 
 function checkLoss() {
   if (player.bank < 1) {
-    alert(
-      "You are out of money, would you like to put a lein on your house to continue playing?"
-    );
+    modal.style.display = "block";
+    // alert(
+    //   "You are out of money, would you like to put a lein on your house to continue playing?"
+    // );
   }
 }
 
+/*--------------------- modal stuff------------------------------*/
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 /*---- plan ---- 
 
 going to try to do this one with objects instead of just arrays
